5ea36c4b2fc2deb3832d671992b3c43b15819719
RassielMex <rhcp94.rm@gmail.com>
2022-09-02 18:41:13 -0500

    colors

---
+++ src/common/barColors.js
@@ -0,0 +1,8 @@
+const colors = {
+  primary: "rgba(75, 120, 12, 0.5)",
+  secondary: "rgba(25, 99, 132, 0.5)",
+  success: "rgba(50, 10, 102, 0.5)",
+  warning: "rgba(150, 20, 120, 0.5)",
+};
+
+export default colors;
