41163823257fee9ce43dd0683b0a035816b010f6
RassielMex <rhcp94.rm@gmail.com>
2022-09-03 12:21:33 -0500

    refactor

--- src/common/fetchData.js
+++ src/common/fetchData.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
-export const getData = (_endPoint, _setStateFunc) => {
-  axios
+export const getData = async (_endPoint, _setStateFunc) => {
+  const data = axios
     .get(_endPoint)
     .then((response) => {
+      //console.log(response.data);
       _setStateFunc(response.data);
     })
     .catch((e) => {
       _setStateFunc([]);
     });
+  return data;
 };
