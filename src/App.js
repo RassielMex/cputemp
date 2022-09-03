300aecec8242416ebc20400c3a437a3935a5f3ba
RassielMex <rhcp94.rm@gmail.com>
2022-09-03 12:21:00 -0500

    coregraph

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import { Container } from "@mui/material";
 import "./App.css";
 import TempGraph from "./components/TempGraph/TempGraph";
 import RamGraph from "./components/RamGraph/RamGraph";
+import CoreGraph from "./components/CoreGraph/CoreGraph";
 
 function App() {
   return (
     <Container sx={{ marginTop: "3rem" }}>
+      <CoreGraph />
       <TempGraph />
       <RamGraph />
     </Container>
