9a387f516d246472c4e0ee6b32926df1dbd4230e
RassielMex <rhcp94.rm@gmail.com>
2022-09-09 18:59:23 -0500

    title

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import CoreGraph from "./components/CoreGraph/CoreGraph";
 function App() {
   return (
     <Container sx={{ marginTop: "3rem" }}>
+      <h1>Test</h1>
       <CoreGraph />
       <TempGraph />
       <RamGraph />
