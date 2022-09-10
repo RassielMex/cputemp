e195ce62af06985a22a52c1623df14eb3028edc3
RassielMex <rhcp94.rm@gmail.com>
2022-09-10 11:23:53 -0500

    title

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import { Container } from "@mui/material";
+import { Container, Typography, useMediaQuery, useTheme } from "@mui/material";
 import "./App.css";
 import TempGraph from "./components/TempGraph/TempGraph";
 import RamGraph from "./components/RamGraph/RamGraph";
 import CoreGraph from "./components/CoreGraph/CoreGraph";
 
 function App() {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
   return (
     <Container sx={{ marginTop: "3rem" }}>
-      <h1>Test</h1>
+      <Typography variant={isMobile ? "h3" : "h2"} textAlign={"center"}>
+        Cpu monitor
+      </Typography>
       <CoreGraph />
       <TempGraph />
       <RamGraph />
